bb0ed65b40f9664ecffe71ed283046978ddaccce
Theosarp <theo.sarp@yahoo.com>
2023-08-11 11:45:28 -0700

    Python import and modules

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    #Prints the result of addition of all arguments.
+    import sys
+
+    total = 0
+    for m in range(len(sys.argv) - 1):
+        total += int(sys.argv[m + 1])
+    print("{}".format(total))
