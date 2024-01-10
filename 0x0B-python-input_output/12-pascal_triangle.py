32d6b99e34207453037369cbfe750b45f38432e9
Theo Akesseh Sarpong <theo.sarp@yahoo.com>
2024-01-09 21:41:29 -0800

    Update

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -6,15 +6,8 @@ def pascal_triangle(n):
     """Returns a list of lists of integers
      representing the Pascal’s triangle of n:
      """
-    if n <= 0:
-        return []
-
-    triangles = [[1]]
-    while len(triangles) != n:
-        tri = triangles[-1]
-        tmp = [1]
-        for i in range(len(tri) - 1):
-            tmp.append(tri[i] + tri[i + 1])
-        tmp.append(1)
-        triangles.append(tmp)
-    return
+      rows = [[1 for y in range(x + 1)] for x in range(n)]
+    for n in range(n):
+        for x in range(n - 1):
+            rows[n][x + 1] = sum(rows[n - 1][x:x + 2])
+    return rows
